1c4e08fdd8bf8202221b3d1ce28c2de5a66dafdb
SeungYeop Yang <seungyeop.yang@ibm.com>
2023-02-14 23:27:26 -0600

    init

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 ./create_locald.sh
 cd packer
 rm -rf output
-PACKER_LOG=0 packer init ubuntu.pkr.hcl
-PACKER_LOG=0 packer build ubuntu.pkr.hcl
+PACKER_LOG=1 packer init fedora.pkr.hcl
+PACKER_LOG=1 packer build fedora.pkr.hcl
